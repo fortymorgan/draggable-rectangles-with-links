90fac92569fd3da18632773b94af92fa86f98437
Forty Morgan <dimazhmudikov@gmail.com>
2018-08-12 20:46:45 +0300

    Add zindex to link points

--- src/components/Point.jsx
+++ src/components/Point.jsx
@@ -24,6 +24,7 @@ export default class Point extends Component {
       top: coords[position].top,
       display: linking.state || hovered ? 'block' : 'none',
       cursor: 'pointer',
+      zIndex: 2,
     };
 
     return (
