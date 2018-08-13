c4aafe0ca90d089c09f5e2c8c4b7b94869cca811
Forty Morgan <dimazhmudikov@gmail.com>
2018-08-14 00:16:11 +0300

    Add react draggable

--- src/components/Rectangle.jsx
+++ src/components/Rectangle.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
+import Draggable from 'react-draggable';
 import Point from '../containers/Point';
 import { params } from '../rectParams';
 
 export default class Rectangle extends Component {
-  state = {
-    dragging: false,
-    hovered: true,
-    diff: {
-      x: 0,
-      y: 0,
-    },
-    blockOnTop: false,
-    blockOnBottom: false,
-    blockOnLeft: false,
-    blockOnRight: false,
+  constructor(props) {
+    super(props);
+
+    const bounds = this.checkBounds();
+
+    this.state = {
+      dragging: false,
+      hovered: true,
+      diff: {
+        x: 0,
+        y: 0,
+      },
+      bounds,
+    }
   }
 
   onStart = ({ clientX, clientY }) => {
@@ -24,82 +28,51 @@ export default class Rectangle extends Component {
   }
 
   onMove = ({ clientX, clientY }) => {
-    const { onRectMove, rect: { id, position } } = this.props;
-    const {
-      diff: { x, y },
-      dragging,
-      blockOnTop,
-      blockOnBottom,
-      blockOnRight,
-      blockOnLeft,
-    } = this.state;
+    const { onRectMove, rect: { id } } = this.props;
+    const { diff, dragging } = this.state;
 
     if (dragging) {
-      let checkX
-      if (blockOnLeft) {
-        checkX = clientX - x > position.x ? clientX - x : position.x;
-      } else if (blockOnRight) {
-        checkX = clientX - x < position.x ? clientX - x : position.x;
-      }
-      
-      let checkY
-      if (blockOnTop) {
-        checkY = clientY - y > position.y ? clientY - y : position.y;
-      } else if (blockOnBottom) {
-        checkY = clientY - y < position.y ? clientY - y : position.y;
-      }
-      
-      this.checkBlock(blockOnRight || blockOnLeft ? checkX : clientX - x, blockOnTop || blockOnBottom ? checkY : clientY - y);
-      const newCenter = { x: blockOnRight || blockOnLeft ? position.x : clientX - x, y: blockOnTop || blockOnBottom ? position.y : clientY - y };
+      const bounds = this.checkBounds()
+
+      this.setState({ bounds });
+      const newCenter = { x: clientX - diff.x, y: clientY - diff.y };
       onRectMove(newCenter, id);
     }
   }
 
-  checkBlock = (x, y) => {
-    const { rects, rect: { id } } = this.props;
-    const otherRects = rects.filter(rect => rect.id != id)
-
-    const blockTop = otherRects.some(({ position }) =>
-      params.height - 5 <= y - position.y &&
-      y - position.y <= params.height + 1 &&
-      Math.abs(position.x - x) < params.width);
-    const blockBottom = otherRects.some(({ position }) =>
-      params.height - 5 <= position.y - y &&
-      position.y - y <= params.height + 1 &&
-      Math.abs(position.x - x) < params.width);
-    const blockRight = otherRects.some(({ position }) =>
-      params.width - 5 <= position.x - x &&
-      position.x - x <= params.width + 1 &&
-      Math.abs(position.y - y) < params.height);
-    const blockLeft = otherRects.some(({ position }) =>
-      params.width - 5 <= x - position.x &&
-      x - position.x <= params.width + 1 &&
-      Math.abs(position.y - y) < params.height);
-
-    this.setState({
-      blockOnTop: blockTop,
-      blockOnBottom: blockBottom,
-      blockOnLeft: blockLeft,
-      blockOnRight: blockRight,
-    })
-  }
-  
-  onStop = ({ clientX, clientY }) => {
-    const { onRectMove, rect: { id, position } } = this.props;
-    const {
-      diff: { x, y },
-      dragging,
-      blockOnTop,
-      blockOnBottom,
-      blockOnRight,
-      blockOnLeft,
-    } = this.state;
-
-    if (dragging) {
-      const newCenter = { x: blockOnRight || blockOnLeft ? position.x : clientX - x, y: blockOnTop || blockOnBottom ? position.y : clientY - y };
-      onRectMove(newCenter, id);
-      this.setState({ dragging: false });
+  checkBounds = () => {
+    const { rect: { id, position: { x, y } }, rects } = this.props;
+    const { innerHeight, innerWidth } = window;
+    const { width, height } = params;
+    const defaultBounds = {
+      left: 0,
+      right: innerWidth - width,
+      top: 0,
+      bottom: innerHeight - height,
     }
+
+    const otherRects = rects.filter(rect => rect.id != id);
+    return otherRects.reduce((acc, { position }) => {
+      if (position.x < x && Math.abs(position.y - y) < height) {
+        const left = Math.max(position.x + width / 2, acc.left);
+        return { ...acc, left };
+      } else if (position.x > x && Math.abs(position.y - y) < height) {
+        const right = Math.min(position.x - 3 * width / 2, acc.right);
+        return { ...acc, right };
+      } else if (position.y < y && Math.abs(position.x - x) < width) {
+        const top = Math.max(position.y + height / 2, acc.top);
+        return { ...acc, top };
+      } else if (position.y > y && Math.abs(position.x - x) < width) {
+        const bottom = Math.min(position.y - 3 * height / 2, acc.bottom)
+        return { ...acc, bottom };
+      } else {
+        return acc;
+      }
+    }, defaultBounds)
+  }
+
+  onStop = () => {
+    this.setState({ dragging: false });
   }
 
   onOver = () => {
@@ -107,42 +80,45 @@ export default class Rectangle extends Component {
   }
 
   onLeave = () => {
-    this.setState({ hovered: false, dragging: false });
+    this.setState({ hovered: false });
   }
 
   render() {
     const { position: { x, y }, color } = this.props.rect;
-    const { dragging, hovered } = this.state;
+    const { dragging, hovered, bounds } = this.state;
     const { width, height } = params;
-  
+
     const style = {
       width,
       height,
       position: 'absolute',
-      left: x - width / 2,
-      top: y - height / 2,
       background: color,
       cursor: dragging ? 'move' : 'default',
     };
-  
+
     return (
-      <div
-        style={style}
-        onMouseDown={this.onStart}
-        onMouseMove={this.onMove}
-        onMouseUp={this.onStop}
-        onMouseOver={this.onOver}
-        onMouseLeave={this.onLeave}
+      <Draggable
+        onStart={this.onStart}
+        onDrag={this.onMove}
+        onStop={this.onStop}
+        position={{ x: x - width / 2, y: y - height / 2 }}
+        bounds={bounds}
       >
-        {['top', 'right', 'bottom', 'left'].map(pos => (
-          <Point
-            key={pos}
-            position={pos}
-            hovered={hovered}
-            rect={this.props.rect}
-          />)
-        )}
-      </div>
+        <div
+          style={style}
+          onMouseOver={this.onOver}
+          onMouseOut={this.onLeave}
+        >
+          {['top', 'right', 'bottom', 'left'].map(pos => (
+            <Point
+              key={pos}
+              position={pos}
+              hovered={hovered}
+              rect={this.props.rect}
+            />)
+          )}
+        </div>
+      </Draggable>
     );
   }
 }
