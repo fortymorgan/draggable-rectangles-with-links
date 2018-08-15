eb3d0311fb9a8552b3a41c9a613ae0e0e3e83730
Forty Morgan <dimazhmudikov@gmail.com>
2018-08-15 14:18:14 +0300

    Change background feedback color

--- src/isOverlapping.js
+++ src/isOverlapping.js
@@ -4,7 +4,7 @@ export default ({ x, y }, rectangles) => {
   const isOver = rectangles.some(({ position }) => Math.abs(x - position.x) < params.width && Math.abs(y - position.y) < params.height);
 
   if (isOver) {
-    document.body.style.background = 'coral';
+    document.body.style.background = 'pink';
     setTimeout(() => document.body.style.background = '', 50);
   }
 
